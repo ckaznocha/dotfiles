c033e4b3d030f6a08278adb89230f67b06f22855
Clifton Kaznocha <ckaznocha@gmail.com>
2022-12-09 16:32:39 -0800

    update install

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 touch ~/.at_work
 ln -s /workspace/container-data/.gitconfig.local ~/.gitconfig.local
+ln -s /workspace/container-data/.zshrc.local ~/.zshrc.local
 git clone https://github.com/andsens/homeshick.git ~/.homesick/repos/homeshick
 source ~/.homesick/repos/homeshick/homeshick.sh
 git -C $HOME/.homesick/repos/dotfiles submodule update --init --recursive
