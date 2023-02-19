c33de19b2239115ccb1df2639b765beafe54cd61
Clifton Kaznocha <ckaznocha@gmail.com>
2023-02-18 19:09:25 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ git -C $HOME/.homesick/repos/dotfiles submodule update --init --recursive
 
 homeshick link -f dotfiles
 
-npm install -g diff-so-fancy
-sudo apt update && sudo apt install -y vim-nox fzf tmux sqlite3
+npm install -g diff-so-fancy tldr
+sudo apt update && sudo apt install -y vim-nox fzf tmux sqlite3 bat fd-find
 
 vim +PlugInstall +qall
